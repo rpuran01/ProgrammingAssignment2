b2a62c271b6336b53d21bce2e3663e57a73f7ae9
rpuran01 <rpuranam.jgd@gmail.com>
2015-05-22 19:22:14 +0530

    ProgrammingAssignment2
    programming assignment 2 - courser R Program class. creates a matrix
    object to cache a matrix and compute the inverse on the matrix from
    cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2
+## 
 
-## Write a short comment describing this function
+## This function takes matrix as an input and creates an object matrix to cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                   set<- function(y){
+                   	     x<<- y
+                   }
+                   get<- function() x
+                   list(set=set, get=get)
 }
 
 
-## Write a short comment describing this function
+## This function computes inverse of the special matrix returned by the function above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<- x$get()
+        if(!is.null(m)){
+        	   message("geting cached data")
+        	   inverse<- solve(m)
+        	   return(inverse)
+        }
+        
+        
 }
